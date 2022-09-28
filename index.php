2bbc1e7d479e64da54e5e6a1dd2894c6df8787f8
divyansh20code <114576230+divyansh20code@users.noreply.github.com>
2022-09-28 15:38:02 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Happy Birthday Betu didi!!</title>
+    <link rel="shortcut icon" href="शीर्षक-रहित डिज़ाइन (35).png" type="image/x-icon">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body class="m-7">
+    <head>
+    <nav class="flex  justify-between my-6">
+        <img src="शीर्षक-रहित डिज़ाइन (35).png" alt="icon" class="w-10 h-10 -my-2 ">
+        <ul class="flex space-x-7">
+            <li>Happy Birthday</li>
+            <li>About You</li>
+            <li>Your Hobbies</li>
+            <li>Your Education</li>
+            <li>Gallery</li>
+        </ul>
+        <i class="fa fa-search"></i>
+    </nav>
+    <hr class="border-xl border-red-600">
+    <div class="banner w-1/2">
+        <div class="">  
+              
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque rem amet culpa reiciendis dignissimos necessitatibus esse. Fugit earum quae facilis obcaecati! Minus porro eos eaque debitis inventore omnis dicta delectus? Sunt beatae iure ipsum dolorem dolore?</p>
+        </div>
+    </div>
+    </head>
+</body>
+</html>
